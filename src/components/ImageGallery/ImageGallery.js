65c1c7ce0e4f8d393f6e60d37696a05acf063aa6
Vasylyna Shvets <104644733+Weselka@users.noreply.github.com>
2023-01-07 17:40:57 +0200

    refactoring ImageGallery
    changes from class ImageGallery to function without use...

--- src/components/ImageGallery/ImageGallery.js
+++ src/components/ImageGallery/ImageGallery.js
@@ -1,21 +1,16 @@
 import PropTypes from 'prop-types';
-import { Component } from 'react';
 import { ImageGalleryItem } from 'components';
 import { ImageGalleryList } from './ImageGallery.styled';
 
-export class ImageGallery extends Component {
-  render() {
-    const { images } = this.props;
-
-    return (
-      <ImageGalleryList>
-        {images.map(image => (
-          <ImageGalleryItem key={image.id} image={image}></ImageGalleryItem>
-        ))}
-      </ImageGalleryList>
-    );
-  }
-}
+export const ImageGallery = ({ images }) => {
+  return (
+    <ImageGalleryList>
+      {images.map(image => (
+        <ImageGalleryItem key={image.id} image={image}></ImageGalleryItem>
+      ))}
+    </ImageGalleryList>
+  );
+};
 
 ImageGallery.propTypes = {
   image: PropTypes.shape({
