a49128e99a11f2ea57ece795d437c7bfcfe6bbfe
Vasylyna Shvets <104644733+Weselka@users.noreply.github.com>
2023-01-08 18:52:40 +0200

    refactoring App
    changes from class to function;
    changes from state to useState;
    add useEffect for async function

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,6 +1,7 @@
 import PropTypes from 'prop-types';
-import { Component } from 'react';
+import { useState, useEffect } from 'react';
 import { ToastContainer } from 'react-toastify';
+import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { fetchImages } from '../../services/Images-api';
 import { Section, ErrorText, ErrorBox, Loader } from 'components';
@@ -13,131 +14,103 @@ import {
   ImageError,
 } from 'components';
 
-export class App extends Component {
-  static propTypes = {
-    state: PropTypes.shape({
-      imagesName: PropTypes.string.isRequired,
-      images: PropTypes.array.isRequired,
-      page: PropTypes.number.isRequired,
-      totalHits: PropTypes.number.isRequired,
-      error: PropTypes.string.isRequired,
-      status: PropTypes.string.isRequired,
-    }),
-  };
-
-  state = {
-    imagesName: '',
-    images: [],
-    page: 1,
-    totalHits: null,
-    error: null,
-    status: 'idle',
-  };
-
-  componentDidUpdate(_, prevState) {
-    const prevName = prevState.imagesName;
-    const nextName = this.state.imagesName;
-    const prevPage = prevState.page;
-    const nextPage = this.state.page;
-
-    // if (prevName !== nextName) {
-    //   this.setState({
-    //     page: 1,
-    //     images: [],
-    //     status: 'idle',
-    //   });
-    // }
-
-    if (prevName !== nextName || prevPage !== nextPage) {
-      this.setState({
-        page: 1,
-        images: [],
-        status: 'idle',
-      });
-      this.getImages(nextName, nextPage);
-    }
-  }
-
-  getImages = async (nextName, page) => {
-    if (!nextName) {
-      return;
-    }
-    try {
-      this.setState({ status: 'pending' });
-      const {
-        hits,
-        total,
-        totalHits,
-        // page: currentPage,
-      } = await fetchImages(nextName, page);
-      if (hits.length === 0) {
-        this.setState({ status: 'idle' });
+export const App = () => {
+  const [imagesName, setImagesName] = useState('');
+  const [images, setImages] = useState([]);
+  const [page, setPage] = useState(1);
+  const [totalHits, setTotalHits] = useState(null);
+  const [error, setError] = useState(null);
+  const [status, setStatus] = useState('idle');
+  
+  useEffect(() => {
+    const getImages = async (imagesName, page) => {
+      if (imagesName === '') {
         return;
       }
-      this.setState(prevState => ({
-        images: [...prevState.images, ...hits],
-        total,
-        totalHits,
-        status: 'resolved',
-      }));
-    } catch (error) {
-      this.setState({ error, status: 'rejected' });
-    }
+      try {
+        setStatus('pending');
+        const {
+          hits,
+          // total,
+          totalHits,
+        } = await fetchImages(imagesName, page);
+        if (hits.length === 0) {
+          toast.info('No images were found for your request');
+          setStatus('idle');
+          return;
+        }
+        setImages(prevState => [...prevState, ...hits]);
+        // setPage(prevPage => prevPage + 1);
+        setTotalHits(totalHits);
+        setStatus('resolved');
+      } catch (error) {
+        setError('rejected');
+      }
+    };
+    getImages(imagesName, page);
+  }, [imagesName, page]);
+
+  const handleSearchFormSubmit = imagesName => {
+    setImagesName(imagesName);
+    setPage(1);
+    setImages([]);
+    setStatus('idle');
   };
 
-  handleSearchFormSubmit = imagesName => {
-    this.setState({ imagesName });
+  const btnLoadMore = () => {
+    setPage(prevPage => prevPage + 1);
   };
 
-  btnLoadMore = () => {
-    this.setState(prevState => ({
-      page: prevState.page + 1,
-    }));
-  };
-
-  render() {
-    const { images, error, status, totalHits, imagesName } = this.state;
-
-    if (status === 'idle') {
-      return (
-        <Section>
-          <Searchbar onSubmit={this.handleSearchFormSubmit} />
-          <ErrorBox>
-            <ErrorText>Add a photo to view or enter another name</ErrorText>
-          </ErrorBox>
-          <ToastContainer autoClose={3000} />
-        </Section>
-      );
-    }
-    if (status === 'pending') {
-      return (
-        <Section>
-          <Loader />
-        </Section>
-      );
-    }
-    if (status === 'rejected') {
-      return (
-        <Section>
-          <Searchbar onSubmit={this.handleSearchFormSubmit} />
-          <Container>
-            <ImageError message={error.message}>{error.message}</ImageError>
-          </Container>
-          <ToastContainer autoClose={3000} />
-        </Section>
-      );
-    }
-    if (status === 'resolved') {
-      return (
-        <Section>
-          <Searchbar onSubmit={this.handleSearchFormSubmit} />
-          <Container>
-            <ImageGallery imagesName={imagesName} images={images} />
-          </Container>
-          {images.length < totalHits && <LoadMore onClick={this.btnLoadMore} />}
-          <ToastContainer autoClose={3000} />
-        </Section>
-      );
-    }
+  if (status === 'idle') {
+    return (
+      <Section>
+        <Searchbar onSubmit={handleSearchFormSubmit} />
+        <ErrorBox>
+          <ErrorText>Add a photo to view or enter another name</ErrorText>
+        </ErrorBox>
+        <ToastContainer autoClose={3000} />
+      </Section>
+    );
   }
-}
+  if (status === 'pending') {
+    return (
+      <Section>
+        <Loader />
+      </Section>
+    );
+  }
+  if (status === 'rejected') {
+    return (
+      <Section>
+        <Searchbar onSubmit={handleSearchFormSubmit} />
+        <Container>
+          <ImageError message={error.message}>{error.message}</ImageError>
+        </Container>
+        <ToastContainer autoClose={3000} />
+      </Section>
+    );
+  }
+  if (status === 'resolved') {
+    return (
+      <Section>
+        <Searchbar onSubmit={handleSearchFormSubmit} />
+        <Container>
+          <ImageGallery imagesName={imagesName} images={images} />
+        </Container>
+        {images.length < totalHits && <LoadMore onClick={btnLoadMore} />}
+        <ToastContainer autoClose={3000} />
+      </Section>
+    );
+  }
+};
+
+App.propTypes = {
+  state: PropTypes.shape({
+    imagesName: PropTypes.string.isRequired,
+    images: PropTypes.array.isRequired,
+    page: PropTypes.number.isRequired,
+    totalHits: PropTypes.number.isRequired,
+    error: PropTypes.string.isRequired,
+    status: PropTypes.string.isRequired,
+  }),
+};
