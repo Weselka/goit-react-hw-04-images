21946ad8a535e3531db02727a24ad3ca98cac57c
Vasylyna Shvets <104644733+Weselka@users.noreply.github.com>
2023-01-08 18:47:29 +0200

    refactoring Modal
    changes from class to function;
    changes from componentDidMount, componentWillUnmount to useEffect;

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,44 +1,79 @@
 import PropTypes from 'prop-types';
-import { Component } from 'react';
+import { useEffect } from 'react';
 import { createPortal } from 'react-dom';
 import { Overlay, ModalBox } from './Modal.styled';
 const modalRoot = document.querySelector('#modal-root');
 
-export class Modal extends Component {
-  static propTypes = {
-    state: PropTypes.shape({
-      handleKeyDown: PropTypes.func.isRequired,
-      handleOverlayClick: PropTypes.func.isRequired,
-    }),
-    children: PropTypes.object.isRequired,
-  };
+export const Modal = ({ children, onClose }) => {
+  useEffect(() => {
+    const handleKeyDown = e => {
+      if (e.code === 'Escape') {
+        onClose();
+      }
+    };
+    document.addEventListener('keydown', handleKeyDown);
+    return () => {
+      document.removeEventListener('keydown', handleKeyDown);
+    };
+  });
 
-  componentDidMount() {
-    window.addEventListener('keydown', this.handleKeyDown);
-  }
-  componentWillUnmount() {
-    window.removeEventListener('keydown', this.handleKeyDown);
-  }
-
-  handleKeyDown = e => {
-    if (e.code === 'Escape') {
-      this.props.onClose();
+  const handleOverlayClick = e => {
+    if (e.currentTarget !== e.target) {
+      onClose();
     }
   };
 
-  // handleOverlayClick = e => {
-  //   if (e.currentTarget !== e.target) {
-  //     this.props.onClose();
-  //   }
-  // };
+  return createPortal(
+    <Overlay onClick={handleOverlayClick}>
+      <ModalBox>{children}</ModalBox>
+    </Overlay>,
+    modalRoot
+  );
+};
 
-  render() {
-    const { children } = this.props;
-    return createPortal(
-      <Overlay onClick={this.handleOverlayClick}>
-        <ModalBox>{children}</ModalBox>
-      </Overlay>,
-      modalRoot
-    );
-  }
-}
+Modal.propTypes = {
+  state: PropTypes.shape({
+    handleKeyDown: PropTypes.func.isRequired,
+    handleOverlayClick: PropTypes.func.isRequired,
+  }),
+  children: PropTypes.object.isRequired,
+};
+
+// export class Modal extends Component {
+//   static propTypes = {
+//     state: PropTypes.shape({
+//       handleKeyDown: PropTypes.func.isRequired,
+//       handleOverlayClick: PropTypes.func.isRequired,
+//     }),
+//     children: PropTypes.object.isRequired,
+//   };
+
+//   componentDidMount() {
+//     window.addEventListener('keydown', this.handleKeyDown);
+//   }
+//   componentWillUnmount() {
+//     window.removeEventListener('keydown', this.handleKeyDown);
+//   }
+
+//   handleKeyDown = e => {
+//     if (e.code === 'Escape') {
+//       this.props.onClose();
+//     }
+//   };
+
+//   handleOverlayClick = e => {
+//     if (e.currentTarget !== e.target) {
+//       this.props.onClose();
+//     }
+//   };
+
+//   render() {
+//     const { children } = this.props;
+//     return createPortal(
+//       <Overlay onClick={this.handleOverlayClick}>
+//         <ModalBox>{children}</ModalBox>
+//       </Overlay>,
+//       modalRoot
+//     );
+//   }
+// }
